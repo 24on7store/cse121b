8726f224b9e5e184dd244af6984f4639329f3d93
24on7store <132136928+24on7store@users.noreply.github.com>
2023-11-12 04:51:09 +0300

    week02

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,38 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Mackison Jean Pierre";
+let currentYear = "2023";
+let profilePicture = "images/mike2.jpg"
+
+/* Step 3 - Element Variables */
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("#picture");
+
+
+
+/* Step 4 - Adding Content */
+nameElement.text.Content = "<strong>${fullName}</strong>";
+
+nameElement.text.Content = "<strong>${currentTear}</strong>";
+imageElement.setAttribute("src", profilePicture);
+
+// const imageElement = document.getElementById("profileImage")
+const name = "Mackison Jean Pierre"
+imageElement
+.setAttribute("alt", "Profile image of ${name}")
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
