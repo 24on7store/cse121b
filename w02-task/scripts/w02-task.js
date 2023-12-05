cdef2836b61f3968d6531c4cd9adcb3b111b95dd
24on7store <132136928+24on7store@users.noreply.github.com>
2023-12-05 16:55:28 +0300

    changes

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -4,69 +4,60 @@
 
 /* Step 2 - Variables */
 let fullName = "Mackison Jean Pierre";
-
 const currentYear = new Date().getFullYear();
-
 const profilePicture = "images/mike2.jpg"
 
 
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById("Mackison Jean Pierre")
-
-const foodElement = document.getElementById("food")
-
+const nameElement = document.getElementById("name")
 const yearElement = document.querySelector("#year");
+const foodElement = document.getElementById("food")
+// const imageElement = document.getElementById("profile-image");
+const imageElement = document.querySelector('img')
 
-const imageElement = document.getElementById("profile-image");
+
 
 /* Step 4 - Adding Content */
-nameElement.innerHTML = "<strong>${fullName}</strong>";
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
 
-imageElement.setAttribute("src", profilePicture);
+// imageElement.setAttribute("src", profilePicture);
+if (imageElement) {
+    imageElement.setAttribute('src', profilePicture);
+    imageElement.setAttribute("alt", `Profile image of ${fullName}`);
+}
 
-imageElement.setAttribute("alt", "Profile image of ${fullName}");
 
 
 /* Step 5 - Array */
-// Declare an array variable to hold your favorite foods
-const favoriteFoods = ['Pizza', 'Ice Cream', 'Burger'];
+// 1- Declare an array variable to hold your favorite foods
+const favoriteFoods = ['Pizza', 'Rice', 'Chicken sauce', 'Beans', 'Salad'];
 
-// Modify the HTML element with the id of food to display your favorite foods array
-foodElement.innerHTML = favoriteFoods.join('<br>');
+// 2- Modify the HTML element with the id of food to display your favorite foods array
+// foodElement.innerHTML = favoriteFoods.join('<br>');
+foodElement.innerHTML = favoriteFoods.join();
 
-// Declare and instantiate a variable to hold another single favorite food item
-const newFavoriteFood = 'Sushi';
+//3- Declare and instantiate a variable to hold another single favorite food item
+const newFavoriteFood = 'Plantain and Fish';
 
-// Add the value stored in this new variable to your favorite food array
+// 4- Add the value stored in this new variable to your favorite food array
 favoriteFoods.push(newFavoriteFood);
 
-// Append the new array values onto the displayed content of the HTML element with the id of food
-foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
+// 5- Append the new array values onto the displayed content of the HTML element with the id of food
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 
-// Check Your Understanding
-// Remove the first element in the favorite food array
+// 6- Remove the first element using the shift() method
 favoriteFoods.shift();
 
-// Append the array output showing the modified array
-foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
+// 7- Again, Append the array output showing the modified array
+// foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 
-// Remove the last element in the favorite food array
+// 8- Remove the last element in the favorite food array
 favoriteFoods.pop();
 
-// Append the array output with this final modified favorite foods array
-foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
-
-
-
-
-
-
-
-
-
-
+// 9- Append the array output with this final modified favorite foods array
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 
 
--- w02-task/scripts/w03-task.js
+++ w02-task/scripts/w03-task.js
@@ -1,108 +1,82 @@
+
 /* LESSON 3 - Programming Tasks */
 
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
+function add(number1, number2) {
+  return number1 + number2;
+}
 
+/* Function Declaration - addNumbers function*/
+function addNumbers() {
+  let addNumber1 = Number(document.querySelector('#add1').value);
+  let addNumber2 = Number(document.querySelector('#add2').value);
+  document.querySelector('#sum').value = add(addNumber1, addNumber2);
+}
 
-/* Function Expression - Subtract Numbers */
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
 
+/*Function Expression - Subtraction Feature*/
+const subtract = function (number1, number2) {
+  return number1 - number2;
+};
 
-/* Arrow Function - Multiply Numbers */
+const subtractNumbers = () => {
+  let subtractNumber1 = Number(document.querySelector('#subtract1').value);
+  let subtractNumber2 = Number(document.querySelector('#subtract2').value);
+  document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
+};
 
+document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
 
-/* Open Function Use - Divide Numbers */
+/*Arrow Functions - Multiplication Feature */
+const multiply = (number1, number2) => number1 * number2;
 
+const multiplyNumbers = () => {
+  let factor1 = Number(document.querySelector('#factor1').value);
+  let factor2 = Number(document.querySelector('#factor2').value);
+  document.querySelector('#product').value = multiply(factor1, factor2);
+};
 
-/* Decision Structure */
+document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
+
+/*Division Feature using a combination of function types */
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+const divideNumbers = () => {
+  let dividend = Number(document.querySelector('#dividend').value);
+  let divisor = Number(document.querySelector('#divisor').value);
+  document.querySelector('#quotient').value = divide(dividend, divisor);
+};
+
+document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
+
+/*Selection Structures - Get Total Due Button */
+document.querySelector('#getTotalDue').addEventListener('click', () => {
+  let subtotal = Number(document.querySelector('#subtotal').value);
+  let applyDiscount = document.querySelector('#member').checked; 
+  let total = applyDiscount ? subtotal * 0.8 : subtotal * 1; 
+  document.querySelector('#total').innerText = `Total Due: $${total.toFixed(2)}`;
+});
 
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
+let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+document.querySelector('#array').innerText = numbersArray;
 
 /* Output Odds Only Array */
+document.querySelector('#odds').innerText = numbersArray.filter(number => number % 2 !== 0);
 
 /* Output Evens Only Array */
+document.querySelector('#evens').innerText = numbersArray.filter(number => number % 2 === 0);
 
 /* Output Sum of Org. Array */
-
+document.querySelector('#sumOfArray').innerText = numbersArray.reduce((sum, number) => sum + number, 0); // Added initial value for sum
 /* Output Multiplied by 2 Array */
-
+document.querySelector('#multiplied').innerText = numbersArray.map(number => number * 2);
 /* Output Sum of Multiplied by 2 Array */
-
-
-
-// Function Declaration - Addition Feature
-function add(number1, number2) {
-    return number1 + number2;
-  }
-  
-  // Function Declaration - addNumbers function
-  function addNumbers() {
-    let addNumber1 = Number(document.querySelector('#add1').value);
-    let addNumber2 = Number(document.querySelector('#add2').value);
-    document.querySelector('#sum').value = add(addNumber1, addNumber2);
-  }
-  
-  document.querySelector('#addNumbers').addEventListener('click', addNumbers);
-  
-  // Function Expression - Subtraction Feature
-  const subtract = function (number1, number2) {
-    return number1 - number2;
-  };
-  
-  const subtractNumbers = () => {
-    let subtractNumber1 = Number(document.querySelector('#subtract1').value);
-    let subtractNumber2 = Number(document.querySelector('#subtract2').value);
-    document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
-  };
-  
-  document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
-  
-  // Arrow Functions - Multiplication Feature
-  const multiply = (number1, number2) => number1 * number2;
-  
-  const multiplyNumbers = () => {
-    let factor1 = Number(document.querySelector('#factor1').value);
-    let factor2 = Number(document.querySelector('#factor2').value);
-    document.querySelector('#product').value = multiply(factor1, factor2);
-  };
-  
-  document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
-  
-  // Division Feature using a combination of function types
-  function divide(number1, number2) {
-    return number1 / number2;
-  }
-  
-  const divideNumbers = () => {
-    let dividend = Number(document.querySelector('#dividend').value);
-    let divisor = Number(document.querySelector('#divisor').value);
-    document.querySelector('#quotient').value = divide(dividend, divisor);
-  };
-  
-  document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
-  
-  // Selection Structures - Get Total Due Button
-  document.querySelector('#getTotalDue').addEventListener('click', () => {
-    let subtotal = Number(document.querySelector('#subtotal').value);
-    let applyDiscount = document.querySelector('#membership').checked;
-    let total = applyDiscount ? subtotal * 0.85 : subtotal;
-    document.querySelector('#total').innerText = `Total Due: $${total.toFixed(2)}`;
-  });
-  
-  // Array Methods - Functional Programming
-  let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-  
-  document.querySelector('#array').innerText = numbersArray;
-  
-  document.querySelector('#odds').innerText = numbersArray.filter(number => number % 2 !== 0);
-  
-  document.querySelector('#evens').innerText = numbersArray.filter(number => number % 2 === 0);
-  
-  document.querySelector('#sumOfArray').innerText = numbersArray.reduce((sum, number) => sum + number);
-  
-  document.querySelector('#multiplied').innerText = numbersArray.map(number => number * 2);
-  
-  document.querySelector('#sumOfMultiplied').innerText = numbersArray.map(number => number * 2)
-    .reduce((sum, number) => sum + number);
-  
+document.querySelector('#sumOfMultiplied').innerText = numbersArray.map(number => number * 2)
+  .reduce((sum, number) => sum + number, 0); 
